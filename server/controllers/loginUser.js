2018af3aa1afc9983c18a8c3db54088b0794cd85
Sofia <sf.hazami@gmail.com>
2025-10-04 19:08:28 +0200

    Login Route

---
+++ server/controllers/loginUser.js
@@ -0,0 +1,33 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    let match = null;
+
+    //verifier password
+    if (user) match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      res.status(401).send("Incorrect Login/Password");
+      return;
+    }
+
+    //Crée un  JWT qui expire au bout de 7j --> le user sera déco au bout de 7j
+    const token = jwt.sign({userId : user.id}, process.env.JWT_SECRET, { expiresIn: '7d' })
+
+    console.log(user, password);
+
+    res.status(200).json({
+        userId : user.id,
+        token
+    });
+    
+  } catch (err) {
+    res.status(500).json({ error: "Erreur serveur" });
+  }
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { registerUser } from '../controllers/registerUser.js';
+import { loginUser } from '../controllers/loginUser.js';
 
 const router = Router();
 
 // POST /auth/register
-router.post('/register', registerUser);   
+router.post('/register', registerUser);
+
+// GET /auth/login
+router.get('/login', loginUser)
 
 export default router;
