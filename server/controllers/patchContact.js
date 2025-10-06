9d40f70a50238b7169539179e1ad836e79fec04f
Sofia <sf.hazami@gmail.com>
2025-10-06 16:57:38 +0200

    Ajout patchContact
    new file:   server/controllers/patchContact.js
    modified:   server/routes/contactRoutes.js
    
    new file:   server/controllers/patchContact.js
    modified:   server/routes/contactRoutes.js

---
+++ server/controllers/patchContact.js
@@ -0,0 +1,22 @@
+import Contact from "../models/Contact.js";
+
+export async function patchContact(req, res) {
+  try {
+    const updates = req.body;
+    const idContact = req.params.id;
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+      idContact,
+      { $set: updates },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedContact) {
+      return res.status(304).json({ message: "Contact not modified" });
+    }
+    res.status(200).json(updatedContact);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+}
